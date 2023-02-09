16c192158ed763f20c5d22dcb8baea43b376358f
Ritik Gaire <ritik123gaire@gmail.com>
2023-02-09 22:10:19 +0545

    seralizer detailed

--- store/serializers.py
+++ store/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
 from decimal import Decimal
-from store.models import Product
+from store.models import Product , Collection
 
-class ProductSerializer(serializers.Serializer):
-    id =  serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6,decimal_places=2,source='unit_price')
+
+class CollectionSeriallizer(serializers.ModelSerializer):
+   class Meta:
+    model = Collection
+    fields = ['id','title']
+
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','title','unit_price','price_with_tax','collection']
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
 
     def calculate_tax(self , product: Product):
--- store/views.py
+++ store/views.py
@@ -7,18 +7,25 @@ from .models import Product
 from .serializers import ProductSerializer
 # Create your views here.
 
-@api_view()
-def product_list(request):
-    queryset  = Product.objects.all()
-    serializer = ProductSerializer(queryset,many=True)
-    return Response(serializer.data)
+@api_view(['GET','POST'])
 
+def product_list(request):
+    if request.method == 'GET':
+        queryset  = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(queryset,many=True,context={'request':request})
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        return Response('ok')
 
 @api_view()
 def product_detail(request,id):
     product=get_object_or_404(Product,pk=id)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
-        
 
+@api_view()        
+def collection_detail(request,pk):
+    return Response('ok')
 # 7 -creating view
